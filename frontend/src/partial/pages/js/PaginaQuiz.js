600258aaabc3b84f0a1dceba7ccb3cb8ae6eda9d
Lutero <marcos09rocha@gmail.com>
2022-05-31 16:38:35 -0300

    fotos sistemas trocadas

--- frontend/src/partial/pages/js/PaginaQuiz.js
+++ frontend/src/partial/pages/js/PaginaQuiz.js
@@ -1,54 +1,59 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import foto1 from "../../img/foto1.jpg";
-import foto2 from "../../img/foto2.jpeg";
-import foto3 from "../../img/foto3.jpg";
-import foto4 from "../../img/foto4.jpeg";
-import foto5 from "../../img/foto5.jpeg";
+import gatinho from "../../img/gatinho.jpeg";
+import foto1 from "../../img/animal-001-j1.jpg";
+import foto2 from "../../img/animal-001-j2.jpg";
+import foto3 from "../../img/animal-001-j3.jpg";
+import foto4 from "../../img/animal-001-j4.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 import "../css/index.css";
+import PaginaInicial from "./PaginaInicial";
 class PaginaQuiz extends React.Component {
+  
   state = {
     selected: 0,
-    imgs: [foto1, foto2, foto3, foto4, foto5],
+    imgs: [gatinho, foto1, foto2, foto3, foto4],
     id: 1,
     cadastro: "25/05/2022",
-    Tempo: 120,
-    pergunta: "pergunta 1",
+    tempo: 120,
+    pergunta: "Tem liquido?",
+    timer: null
   };
-  
-  startTime(){
-    setTimeout(() => {
-    if(this.state.Tempo> 0){
-        this.setState(this.state.Tempo -=1)}
-    },1000)
+
+  flag = false;
+  componentDidMount(){
+    if (!this.flag) this.setState({timer: setInterval(() => {
+      if (this.state.tempo > 0) {
+       this.setState(state => ({tempo: state.tempo - 1}))
+      }
+    }, 1000) });
+    this.flag = true;
   }
- 
-  formatarTempo(time){
 
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+  componentWillUnmount(){
+    if (this.state.timer)
+    clearInterval(this.state.timer);
+  }
+
+
+  formatarTempo() {
+    const minutes = Math.floor(this.state.tempo / 60);
+    let seconds = this.state.tempo % 60;
     if (seconds < 10) {
-        seconds = `0${seconds}`;
+      seconds = `0${seconds}`;
     }
-
     return `${minutes}:${seconds}`;
   }
-
- 
 
   render() {
     return (
       <>
         <div className="row mt-5 div_principal">
-          <div
-            className="col-6 pt-4 text-center"
-            style={{ border: "1px solid black" }}
-          >
+          <div className="col-6 pt-4 text-center">
             <div className="">
               <div className="botoes">
                 <Button>
@@ -62,15 +67,7 @@ class PaginaQuiz extends React.Component {
               </div>
             </div>
             <div
-              style={{
-                width: 200,
-                heigth: 200,
-                position: "absolute",
-                left: 200,
-                top: 200,
-                opacity: 0.3,
-              }}
-              className="bg-danger norte"
+              className="norte"
               onClick={() => {
                 this.setState({ selected: 1 });
               }}
@@ -78,15 +75,7 @@ class PaginaQuiz extends React.Component {
               .
             </div>
             <div
-              style={{
-                width: 200,
-                heigth: 200,
-                position: "absolute",
-                left: 550,
-                top: 200,
-                opacity: 0.3,
-              }}
-              className="bg-success nordeste"
+              className="nordeste"
               onClick={() => {
                 this.setState({ selected: 2 });
               }}
@@ -94,15 +83,7 @@ class PaginaQuiz extends React.Component {
               .
             </div>
             <div
-              style={{
-                width: 200,
-                heigth: 200,
-                position: "absolute",
-                left: 200,
-                top: 400,
-                opacity: 0.3,
-              }}
-              className="bg-info sul"
+              className="sul"
               onClick={() => {
                 this.setState({ selected: 3 });
               }}
@@ -110,60 +91,45 @@ class PaginaQuiz extends React.Component {
               .
             </div>
             <div
-              style={{
-                width: 200,
-                heigth: 200,
-                position: "absolute",
-                left: 550,
-                top: 400,
-                opacity: 0.3,
-              }}
-              className="bg-warning suldeste"
+              className="sudeste"
               onClick={() => {
                 this.setState({ selected: 4 });
               }}
             >
               .
             </div>
-            <img src={foto1} alt="" />
+            <img src={gatinho} alt="" />
             <div className="informacoes_img">
               <p> id: {this.state.id}</p>
               <p>cadastro: {this.state.cadastro}</p>
             </div>
           </div>
-          <div
-            className="col-6 pt-4 text-center"
-            style={{ border: "1px solid black" }}
-          >
-            <div className="img-return" style={{ border: "1px solid black" }}>
-              <img src={this.state.imgs[this.state.selected]} />
+          <div className="col-6 pt-4 text-center">
+            <div className="botao_voltar">
+              <Button
+              onClick={() => this.props.setPagina(PaginaInicial)}
+              >Voltar</Button>
+            </div>
+            <div className="img-return">
+              <img
+              alt=""
+                src={this.state.imgs[this.state.selected]}
+              />
             </div>
             <div>
-              <p>
-                Tempo:{this.formatarTempo(this.state.Tempo)}
-                
-              </p>
+              <p>Tempo:{this.formatarTempo()}</p>
             </div>
             <div className="pergunta">
               <h4>{this.state.pergunta}</h4>
             </div>
             <div className="resposta">
-              <Container>
+              <Container className="mt-3">
                 <Row className="mb-5">
                   <Col xs={6}>
-                    <Button>resposta 1</Button>
+                    <Button>Não</Button>
                   </Col>
                   <Col xs={6}>
-                    <Button>resposta 2</Button>
-                  </Col>
-                </Row>
-
-                <Row>
-                  <Col xs={6}>
-                    <Button>resposta 3</Button>
-                  </Col>
-                  <Col xs={6}>
-                    <Button>resposta 4</Button>
+                    <Button>Sim</Button>
                   </Col>
                 </Row>
               </Container>
